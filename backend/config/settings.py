1950fffb07d08f3b9a5895ad3ac38523ea4764f8
kim <codewithkim@gmail.com>
2026-09-01 11:08:16 -0700

    feat: initialize backend infrastructure with Docker Compose and Celery

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -10,27 +10,22 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 import os
-from dotenv import load_dotenv
-load_dotenv()
-
 from pathlib import Path
+from dotenv import load_dotenv
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR.parent / '.env')
 
 SECRET_KEY = os.getenv("SECRET_KEY")
+if not SECRET_KEY:
+    raise ValueError("The SECRET_KEY environment variable is missing from your .env file!")
 
 DEBUG = os.getenv("DEBUG") == "True"
 CELERY_BROKER_URL = os.getenv("REDIS_URL")
 CELERY_RESULT_BACKEND = os.getenv("REDIS_URL")
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-oh8$f9$s_k5k2@)o9v$ziyuc$@fk2f*!&loo4te!#()7(-qbo5'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
